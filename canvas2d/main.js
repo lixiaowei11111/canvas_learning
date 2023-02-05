a305c3019140ebee7a678b56934d79e804f52eda
lxw <wb.lixiaowei01@mesg.corp.netease.com>
2023-02-05 18:41:28 +0800

    no message

--- canvas2d/main.js
+++ canvas2d/main.js
@@ -2,3 +2,4 @@ export * from "./src/index.js";
 export * from "./src/002_draw_path.js";
 export * from "./src/003_style_color.js";
 export * from "./src/004_text.js";
+export * from "./src/005_clip.js";
